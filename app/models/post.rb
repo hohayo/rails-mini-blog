7fe2ce247baf0bd57bd656a9e61de29a89d17cd0
Kyle Chuang <hohayo@gmail.com>
2024-10-31 20:47:45 +0800

    啟用模型搜索和過濾功能

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :author
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "author_id", "body", "created_at", "id", "published_at", "title", "updated_at" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "author" ]
+  end
 end
