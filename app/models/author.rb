b8a00158a5d8d924242d341f5f2f6b3a8ae9787e
Kyle Chuang <hohayo@gmail.com>
2024-11-01 22:51:49 +0800

    建立1對多關聯

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
   has_one_attached :avatar do |attachable|
     attachable.variant :square, resize_to_fill: [ 250, 250 ]
     attachable.variant :thumb, resize_to_fill: [ 100, 100 ]
@@ -7,5 +9,9 @@ class Author < ApplicationRecord
 
   def self.ransackable_attributes(auth_object = nil)
     [ "created_at", "email", "id", "name", "updated_at" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "posts" ]
   end
 end
