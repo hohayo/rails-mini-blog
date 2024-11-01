97cd134a069e551a066c6161348cf13014aa6bc8
Kyle Chuang <hohayo@gmail.com>
2024-11-01 20:03:58 +0800

    更新密碼

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,20 +1,24 @@
 ActiveAdmin.register User do
   menu parent: :settings, priority: 1
 
-  permit_params :email, :password, :password_confirmation, :avatar
+  permit_params :email, :password, :password_confirmation, :avatar, :current_password
 
   controller do
     def update
-      if params[:user][:password].blank?
-        params[:user].delete(:password)
-        params[:user].delete(:password_confirmation)
-      end
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
 
       if resource.update(permitted_params[:user])
         redirect_to admin_users_path, notice: "更新成功"
       else
         render :edit
       end
+    end
+
+    private
+
+    def password_params
+      params.require(:user).permit(:current_password, :password, :password_confirmation)
     end
   end
 
@@ -35,6 +39,20 @@ ActiveAdmin.register User do
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
+
+  member_action :change_password, method: [ :get, :patch ] do
+    if request.patch?
+      if params[:user][:password].blank?
+        resource.errors.add(:password, "不能為空")
+        render :change_password
+      elsif resource.update_with_password(password_params)
+        bypass_sign_in(resource) if current_user == resource
+        redirect_to admin_user_path(resource), notice: "密碼更新成功"
+      else
+        render :change_password
+      end
+    end
+  end
 
   show do
     attributes_table_for(resource) do
@@ -65,4 +83,7 @@ ActiveAdmin.register User do
     f.actions
   end
 
+  action_item :change_password, only: [ :show, :edit ]do
+    link_to "更新密碼", change_password_admin_user_path(resource), class: "action-item-button"
+  end
 end
